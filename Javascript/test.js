55ede71125c75d509c3a6ad03cf77d9b5adce383
John Doe <johndoe@email.com>
2024-10-30 12:40:35 +0100

    s

---
+++ Javascript/test.js
@@ -0,0 +1,17 @@
+//for løkker
+
+/*for(startverdi; betingelse; økning/dekremet) {
+
+}*/
+
+let betingelse = true
+let tall = 0
+let i = 0
+while(tall < 100) {
+
+    tall = tall +1;
+    console.log("tall er " + tall)
+    i += tall
+}
+
+console.log(i)

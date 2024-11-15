765ee7909f69360236b9de811e4d6e6418b29b41
John Doe <johndoe@email.com>
2024-11-15 10:47:58 +0100

    s

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,5 +1,11 @@
-let tall = [1,2,3,4,5];
-
-for (let i = 0; i < 5; i++) {
-    console.log(tall[i])
+function sumArray(numbers) {
+    let sum = 0
+    for(let i = 0; i < numbers.length; i += 1){
+        console.log(numbers[i])
+        sum = sum + numbers[i]
+    }
+    return sum
 }
+
+let list = [1, 2, 3, 4, 5];
+console.log(sumArray(list))

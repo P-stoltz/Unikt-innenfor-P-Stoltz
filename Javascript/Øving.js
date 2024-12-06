1d38836c9eced8e0daba792ecdc69c8627dfa89d
John Doe <johndoe@email.com>
2024-12-06 09:45:08 +0100

    s

--- Javascript/Øving.js
+++ Javascript/Øving.js
@@ -9,6 +9,25 @@ for (let i = 1; i <= 50; i += 1){
         console.log(i + "buzz")
     }
     else {
-        console.log(i)
+
     }
 }
+
+function kalkuler(tall1, tall2, operasjon) {
+    switch (operasjon) {
+        case '+':
+            return tall1 + tall2;
+        case '-':
+            return tall1 - tall2;
+        case '*':
+            return tall1 * tall2;
+        case '/':
+            return tall2 !== 0 ? tall1 / tall2 : 'Kan ikke dele på 0';
+        default:
+            return 'Ugyldig operasjon';
+    }
+}
+
+// Eksempelbruk
+const resultat = kalkuler(8, 4, '*');
+console.log(resultat); // Skriver ut 32

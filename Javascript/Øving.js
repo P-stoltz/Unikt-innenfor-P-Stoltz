4bb242ccdf9c2d81fca26bca831784240dd9efb0
John Doe <johndoe@email.com>
2024-12-09 08:51:21 +0100

    s

--- Javascript/Øving.js
+++ Javascript/Øving.js
@@ -1,33 +1,11 @@
-for (let i = 1; i <= 50; i += 1){
-    if (i % 3 === 0 & i % 5 === 0) {
-        console.log(i + "Fizzbuzz")
-    }
-    else if (i % 3 === 0) {
-        console.log(i + "Fizz")
-    } 
-    else if (i % 5 === 0) {
-        console.log(i + "buzz")
-    }
-    else {
+let a = [1, 2, 3, 4, 6];
 
+function kul(s) {
+    let b = ''; // Start med en tom streng
+    for (let i = 0; i < s.length; i += 2) { // Gå gjennom hvert andre element i arrayet
+        b = b + s[i]; // Bruk elementene fra input-arrayet s
     }
+    return b; // Returner den ferdige strengen
 }
 
-function kalkuler(tall1, tall2, operasjon) {
-    switch (operasjon) {
-        case '+':
-            return tall1 + tall2;
-        case '-':
-            return tall1 - tall2;
-        case '*':
-            return tall1 * tall2;
-        case '/':
-            return tall2 !== 0 ? tall1 / tall2 : 'Kan ikke dele på 0';
-        default:
-            return 'Ugyldig operasjon';
-    }
-}
-
-// Eksempelbruk
-const resultat = kalkuler(8, 4, '*');
-console.log(resultat); // Skriver ut 32
+console.log(kul(a)); // Kaller funksjonen med arrayet 'a' som input

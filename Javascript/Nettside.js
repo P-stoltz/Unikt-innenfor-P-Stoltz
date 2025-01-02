bb1bec2f635914af7a60cdee1276011d4b4994f8
John Doe <johndoe@email.com>
2025-01-02 20:47:57 +0100

    s

---
+++ Javascript/Nettside.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.getElementById("toggle-button");
+    const sidenav = document.querySelector(".sidenav");
+  
+    toggleButton.addEventListener("click", () => {
+      sidenav.classList.toggle("open");
+      toggleButton.classList.toggle("active"); // Legger til/fjerner 'active' klassen
+      toggleButton.textContent = sidenav.classList.contains("open") ? "×" : "☰";
+    });
+  });
+  

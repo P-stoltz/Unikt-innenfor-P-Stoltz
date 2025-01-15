0f8474f834d8c5172cdc3bb2b5ca897f4de58eec
P-stoltz <peterstoltz2008@gmail.com>
2025-01-15 10:52:21 +0100

    Add files via upload

--- Javascript/Nettside.js
+++ Javascript/Nettside.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.getElementById("toggle-button");
-    const sidenav = document.querySelector(".sidenav");
-  
-    toggleButton.addEventListener("click", () => {
-      sidenav.classList.toggle("open");
-      toggleButton.classList.toggle("active"); // Legger til/fjerner 'active' klassen
-      toggleButton.textContent = sidenav.classList.contains("open") ? "×" : "☰";
-    });
+  const toggleButton = document.getElementById("toggle-button");
+  const sidenav = document.querySelector(".sidenav");
+
+  toggleButton.addEventListener("click", () => {
+    sidenav.classList.toggle("open");
+    toggleButton.classList.toggle("active"); // Legger til/fjerner 'active' klassen
+    toggleButton.textContent = sidenav.classList.contains("open") ? "×" : "☰";
   });
-  
+});
